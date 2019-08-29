13de127f007a98a88890bc7becace0fef7cb928a
Ice_trooper <trooperice@gmail.com>
2019-08-29 15:12:49 +0200

    help me with board and ui please

---
+++ HelpMe/Assets/Scripts/Board.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Rect board;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(board.center, board.size);
+    }
+}
